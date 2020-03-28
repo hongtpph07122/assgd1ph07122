806f9c3a7c945402f19facd3b22af1eef3911969
hongtpph07122 <tranphuchong1435@gmail.com>
2020-03-28 11:07:57 +0700

    5 files added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { ProductService } from './service/product.service';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,  AppRoutingModule],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, AdminComponent ],
   bootstrap:    [ AppComponent ],
   providers: [ProductService]
 })
